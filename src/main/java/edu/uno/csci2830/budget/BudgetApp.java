9344d410659b21cf56c2f4b4b2a7d13de5d02a96
jalen-hobbs <jhobbs3722@gmail.com>
2026-04-23 19:02:06 -0500

    GUI - initial imports

--- src/main/java/edu/uno/csci2830/budget/BudgetApp.java
+++ src/main/java/edu/uno/csci2830/budget/BudgetApp.java
@@ -1,4 +1,11 @@
 package edu.uno.csci2830.budget;
 
+import javafx.application.Application;
+import javafx.collections.FXCollections;
+import javafx.scene.Scene;
+import javafx.scene.control.*;
+import javafx.scene.layout.VBox;
+import javafx.stage.Stage;
+
 public class BudgetApp {
 }
